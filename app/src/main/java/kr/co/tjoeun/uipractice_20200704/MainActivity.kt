65f6b342434ea039f6b6085c9192eecd84226953
김문기 <heartboy08@nate.com>
2020-07-04 14:49:49 +0900

    마진 패딩

--- app/src/main/java/kr/co/tjoeun/uipractice_20200704/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/uipractice_20200704/MainActivity.kt
@@ -2,11 +2,14 @@ package kr.co.tjoeun.uipractice_20200704
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 }
